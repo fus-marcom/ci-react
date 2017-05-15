1d3a23d979cba3b5361c558cd3b6d942ad98522f
Jesse Weigel <jesseweigel@gmail.com>
2017-05-15 18:27:10 -0400

    Add oval semi-transparent background to page titles. Convert title css to styled-jsx

--- components/Title.js
+++ components/Title.js
@@ -1,11 +1,26 @@
 import React, { Component } from 'react';
 
+const hStyles = {
+
+}
+
 class Title extends Component {
 
   render() {
     return (
-      <section className="interior-page-title valign-wrapper" style={{backgroundImage: `linear-gradient(rgba(255,255,255, .5), rgba(255,255,255, .5)), url("${this.props.imgPath}")`, backgroundPositionY: `calc(40vh - 109px), ${this.props.posY}`, backgroundAttachment: 'scroll, fixed'}}>
-        <h1 className="center light valign" style={{paddingTop: 'calc(40vh - 106px)'}}>{this.props.title}</h1>
+      <section className="interior-page-title valign-wrapper" style={{backgroundImage: `url("${this.props.imgPath}")`, backgroundPositionY: `${this.props.posY}`, backgroundAttachment: 'fixed'}}>
+        <h1 className="center light valign">{this.props.title}</h1>
+        <style jsx>{`
+          h1 {
+            background-image: url(/static/img/squiggly1.png), linear-gradient(rgba(255, 255, 255, 0.498039), rgba(255, 255, 255, 0.498039));
+            background-repeat: no-repeat;
+            background-position: 50% 85px, 50%;
+            border-radius: 50%;
+            margin: 0 auto;
+            font-size: 54px;
+            padding: 27px;
+          }
+        `}</style>
       </section>
     );
   }
