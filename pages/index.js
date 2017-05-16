c25d97405ceb97e1abf7ac3361ac845e09ae3868
Jesse R Weigel <jesse@jesseweigel.com>
2017-05-16 10:41:25 -0400

    Use Allure font for hero section text.

--- pages/index.js
+++ pages/index.js
@@ -23,56 +23,58 @@ export default class extends React.Component {
           <link href="https://fonts.googleapis.com/css?family=Cinzel" rel="stylesheet" />
           <link href="https://fonts.googleapis.com/css?family=Tangerine" rel="stylesheet" />
           <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
+          <link href="https://fonts.googleapis.com/css?family=Allura" rel="stylesheet" />
           <link href="/static/css/materialize.css" rel="stylesheet" />
           <link href="/static/css/style.css" rel="stylesheet" />
+
         </Head>
         <Nav />
         <main>
-          <Hero />
-          <StickyNav />
+        <Hero />
+        <StickyNav />
           <div className="section valign-wrapper black-text white-background-flourish" style={{padding: '60px 0'}}>
-            <div className="valign container">
-              <div className="row">
-                <div className="col s12">
-                  <h2 className="light center">Building the Church</h2>
-                </div>
-              </div>
-              <div className="row hide-on-med-and-down">
-                <div className="col s12 m4">
-                  <div className="icon-block">
-                    <h4 className="light">Conferences</h4>
-                    <p className="flow-text">Franciscan University offers unique and inspiring conference experiences.</p>
-                  </div>
-                </div>
-                <div className="col s12 m4">
-                  <div className="icon-block">
-                    <h4 className="light">Workshops</h4>
-                    <p className="flow-text">Explore our rich and varied corpus of many hundreds of workshops for all areas of ministry with <a href="http://www.franciscanathome.com/" title="Franciscan at Home" target="_blank">Franciscan at Home</a>.</p>
-                  </div>
-                </div>
-                <div className="col s12 m4">
-                  <div className="icon-block">
-                    <h4 className="light">Magazine</h4>
-                    <p className="flow-text">Take a look at the latest issue of our long-running magazine,
-                      <a href="https://review.catechetics.com/" title="The Catechetical Review" target="_blank">The Catechetical Review</a>.</p>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="section valign-wrapper white-text black" id="san-damiano">
-            <div className="valign container">
-              <div className="row light flow-text">
-                <div className="col s12 m6">
-                </div>
-                <div className="col s12 m6">
-                  <h2 className="light">Mission</h2>
-                  <p className="flow-text">
-                    The Franciscan University Catechetical Institute serves Christ in His Church by forming those who form others. It supports clergy, parents, and all those responsible for the work of evangelization and catechesis through the provision of courses, conferences, advice, and resources. In fidelity to the primary sources of faith and the pastoral wisdom of the Church's magisterium, the Institute's expert faculty and experienced field mentors seek to be guided by the Holy Spirit as they make these riches available for all whom Christ is gathering into one by his love, making disciples of all nations.
-                  </p>
-                </div>
-              </div>
-            </div>
+           <div className="valign container">
+             <div className="row">
+               <div className="col s12">
+                 <h2 className="light center">Building the Church</h2>
+               </div>
+             </div>
+             <div className="row hide-on-med-and-down">
+               <div className="col s12 m4">
+                 <div className="icon-block">
+                   <h4 className="light">Conferences</h4>
+                   <p className="flow-text">Franciscan University offers unique and inspiring conference experiences.</p>
+                 </div>
+               </div>
+               <div className="col s12 m4">
+                 <div className="icon-block">
+                   <h4 className="light">Workshops</h4>
+                   <p className="flow-text">Explore our rich and varied corpus of many hundreds of workshops for all areas of ministry with <a href="http://www.franciscanathome.com/" title="Franciscan at Home" target="_blank">Franciscan at Home</a>.</p>
+                 </div>
+               </div>
+               <div className="col s12 m4">
+                 <div className="icon-block">
+                   <h4 className="light">Magazine</h4>
+                   <p className="flow-text">Take a look at the latest issue of our long-running magazine,
+                    <a href="https://review.catechetics.com/" title="The Catechetical Review" target="_blank">The Catechetical Review</a>.</p>
+                 </div>
+               </div>
+             </div>
+           </div>
+         </div>
+         <div className="section valign-wrapper white-text black" id="san-damiano">
+           <div className="valign container">
+             <div className="row light flow-text">
+               <div className="col s12 m6">
+               </div>
+               <div className="col s12 m6">
+                 <h2 className="light">Mission</h2>
+                 <p className="flow-text">
+                   The Franciscan University Catechetical Institute serves Christ in His Church by forming those who form others. It supports clergy, parents, and all those responsible for the work of evangelization and catechesis through the provision of courses, conferences, advice, and resources. In fidelity to the primary sources of faith and the pastoral wisdom of the Church's magisterium, the Institute's expert faculty and experienced field mentors seek to be guided by the Holy Spirit as they make these riches available for all whom Christ is gathering into one by his love, making disciples of all nations.
+                 </p>
+               </div>
+             </div>
+           </div>
           </div>
           <div className="section banner valign-wrapper red-background-flourish" id="banner">
             <div className="valign container">
@@ -89,7 +91,7 @@ export default class extends React.Component {
               </div>
               <div className="row light">
                 <div className="col s12 m6">
-                  <TextRevealImageCard />
+                    <TextRevealImageCard />
                 </div>
                 <div className="col s12 m6">
                   <TextRevealImageCard />
@@ -98,16 +100,15 @@ export default class extends React.Component {
             </div>
           </div>
           <div className="section valign-wrapper black-text white-background-flourish">
-            <div className="valign container">
-              <div className="row">
-                <div className="col s12">
-                  <ul className="tabs" style={{backgroundColor: 'transparent'}}>
-                    <li className="tab col s6"><a href="#">Announcements</a></li>
-                    <li className="tab col s6"><a className="active" href="#">Newest Workshops</a></li>
-                  </ul>
-                </div>
+           <div className="valign container">
+             <div className="row">
+               <div className="col s12">
+                 <ul className="tabs" style={{backgroundColor: 'transparent'}}>
+                  <li className="tab col s6"><a href="#">Announcements</a></li>
+                  <li className="tab col s6"><a className="active" href="#">Newest Additions</a></li>
+                </ul>
+               </div>
              </div>
-
              <div className="row">
                <div className="col s12 m6 l4 xl3">
                  <TextCard />
