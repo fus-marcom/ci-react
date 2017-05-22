5b3bd5dc7c2ee0520e2a082d59e697fb850888a5
Jesse R Weigel <jesse@jesseweigel.com>
2017-05-22 13:22:22 -0400

    Create error component

---
+++ components/Error404.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import StickyNav from '../components/StickyNav';
+import Title from '../components/Title';
+
+class Error404 extends Component {
+
+  render() {
+    return (
+      <main>
+        <StickyNav />
+        <div className="container">
+          <div className="section">
+            <div className="row">
+              <h4>No content exists at this address. Please try another page.</h4>
+            </div>
+          </div>
+        </div>
+      </main>
+    );
+  }
+}
+
+export default Error404;
