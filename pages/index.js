4f21ce8a071553b82a442636ed8b0ea3a08e38db
Jesse R Weigel <jesse@jesseweigel.com>
2017-05-24 11:45:05 -0400

    Add new text content to home page

--- pages/index.js
+++ pages/index.js
@@ -42,28 +42,10 @@ export default class extends React.Component {
            <div className="valign container">
              <div className="row">
                <div className="col s12">
-                 <h2 className="light center">Building the Church</h2>
-               </div>
-             </div>
-             <div className="row hide-on-med-and-down">
-               <div className="col s12 m4">
-                 <div className="icon-block">
-                   <h4 className="light">Conferences</h4>
-                   <p className="flow-text">Franciscan University offers unique and inspiring conference experiences.</p>
-                 </div>
-               </div>
-               <div className="col s12 m4">
-                 <div className="icon-block">
-                   <h4 className="light">Workshops</h4>
-                   <p className="flow-text">Explore our rich and varied corpus of many hundreds of workshops for all areas of ministry with <a href="http://www.franciscanathome.com/" title="Franciscan at Home" target="_blank">Franciscan at Home</a>.</p>
-                 </div>
-               </div>
-               <div className="col s12 m4">
-                 <div className="icon-block">
-                   <h4 className="light">Magazine</h4>
-                   <p className="flow-text">Take a look at the latest issue of our long-running magazine,
-                    <a href="https://review.catechetics.com/" title="The Catechetical Review" target="_blank">The Catechetical Review</a>.</p>
-                 </div>
+                 <h2 className="light center">Mission</h2>
+                 <p className="flow-text">
+                   The Franciscan University Catechetical Institute forms Catholics who form others in the Faith. Through courses, conferences, advice, and resources, the Institute supports clergy, parents, and all those responsible for the work of catechesis and evangelization, as they carry out Christ’s command to make disciples of all nations.
+                 </p>
                </div>
              </div>
            </div>
@@ -74,10 +56,10 @@ export default class extends React.Component {
                <div className="col s12 l6">
                </div>
                <div className="col s12 l6">
-                 <h2 className="light">Mission</h2>
-                 <p className="flow-text">
-                   The Franciscan University Catechetical Institute serves Christ in His Church by forming those who form others. It supports clergy, parents, and all those responsible for the work of evangelization and catechesis through the provision of courses, conferences, advice, and resources. In fidelity to the primary sources of faith and the pastoral wisdom of the Church's magisterium, the Institute's expert faculty and experienced field mentors seek to be guided by the Holy Spirit as they make these riches available for all whom Christ is gathering into one by his love, making disciples of all nations.
-                 </p>
+                 <blockquote className="flow-text" style={{borderLeft: '5px solid #a61f26'}}>
+                   “The definitive aim of catechesis is to put people not only in touch, but in communion and intimacy, with Jesus Christ: only He can lead us to the love of the Father in the Spirit and make us share in the life of the of the Holy Trinity.”
+                   —<cite>Saint John Paul II, Catechesi Tradendae, No. 5</cite>
+                 </blockquote>
                </div>
              </div>
            </div>
