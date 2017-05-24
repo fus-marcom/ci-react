e806d2fbb7b5574c9cc592943b631797464fb3df
Jesse R Weigel <jesse@jesseweigel.com>
2017-05-24 13:12:21 -0400

    Test prefetch on sticky nav links

--- components/StickyNav.js
+++ components/StickyNav.js
@@ -11,12 +11,12 @@ class StickyNav extends Component {
     return (
       <div className="section bottom-nav">
         <ul className="hide-on-med-and-down">
-          <li><Link href="/about" title="About"><a>About</a></Link></li>
-          <li><Link href="/team" title="Team"><a>Team</a></Link></li>
-          <li><Link href="/courses" title="Courses"><a>Courses</a></Link></li>
-          <li><Link href="/events" title="Events"><a>Events</a></Link></li>
-          <li><Link href="/resources" title="Resources"><a>Resources</a></Link></li>
-          <li><Link href="/news" title="News"><a>News</a></Link></li>
+          <li><Link prefetch href="/about" title="About"><a>About</a></Link></li>
+          <li><Link prefetch href="/team" title="Team"><a>Team</a></Link></li>
+          <li><Link prefetch href="/courses" title="Courses"><a>Courses</a></Link></li>
+          <li><Link prefetch href="/events" title="Events"><a>Events</a></Link></li>
+          <li><Link prefetch href="/resources" title="Resources"><a>Resources</a></Link></li>
+          <li><Link prefetch href="/news" title="News"><a>News</a></Link></li>
         </ul>
       </div>
     );
