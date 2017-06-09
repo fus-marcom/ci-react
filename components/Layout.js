fb50a7da5ef5001b7d4f4fa13db9751d2a23883f
Vinay Puppal <me@vinaypuppal.com>
2017-06-09 23:23:05 +0530

    add Layout component and move all common code here

--- components/Layout.js
+++ components/Layout.js
@@ -1,16 +1,26 @@
-import React, { Componenet } from 'react';
-import Head from '../components/Head'
+import React, { Component } from 'react';
+import Head from './Head'
+import Nav from './Nav';
+import Footer from './Footer'
 import { initGA } from '../utils/analytics';
 
 export default class Layout extends Component {
   componentDidMount () {
-    initGA();
+    if (!window.GA_INTIALIZED) {
+      initGA();
+      window.GA_INTIALIZED = true;
+    }
   }
   render () {
     return (
       <div style={{display: 'flex', flexDirection: 'column'}}>
         <Head title={this.props.title} description={this.props.description} />
+        <Nav headerType={this.props.headerType} />
         {this.props.children}
+        <Footer />
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.2/js/materialize.min.js"></script>
+        <script src="static/js/app.js"></script>
       </div>
     );
   }
