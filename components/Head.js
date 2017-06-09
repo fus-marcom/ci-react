bed8791595ea7ee90222066086f0f22ca014ccc0
Vinay Puppal <me@vinaypuppal.com>
2017-06-09 23:22:33 +0530

    add local Head component

--- components/Head.js
+++ components/Head.js
@@ -3,8 +3,8 @@ import Head from 'next/head';
 export default ({title, description}) => (
   <Head>
     <meta charset="utf-8" />
-    <title dangerouslySetInnerHTML={{__html : title}}></title>
-          <meta name="description" content={{__html : description}} />
+    <title>{title}</title>
+    <meta name="description" content={description} />
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     <link rel="shortcut icon" href="/static/favicon.ico" />
     <link href="https://fonts.googleapis.com/css?family=Cinzel" rel="stylesheet" />
