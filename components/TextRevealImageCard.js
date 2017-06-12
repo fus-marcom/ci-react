60d5f98e42529de8cc4f6ce9e843b00d220c9ea7
Jesse R Weigel <jesse@jesseweigel.com>
2017-06-12 15:56:18 -0400

    Remove card-action class from 'more' link

--- components/TextRevealImageCard.js
+++ components/TextRevealImageCard.js
@@ -16,7 +16,7 @@ class TextRevealImageCard extends Component {
           <p>
             {this.props.cardContent}
           </p>
-          <div className="card-action">
+          <div>
             <Link prefetch href={this.props.url} title={this.props.cardTitle}><a>More</a></Link>
 
           </div>
