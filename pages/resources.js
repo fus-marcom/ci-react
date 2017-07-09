ba0417583574149f58c3fb86593371c0ab0472bd
Andrew Luca <iamandrewluca@users.noreply.github.com>
2017-07-09 11:49:31 +0300

    Close keyboard on mobile when search is clicked
    I added a `form` wrap around input because it can catch when action icon on keybord is clicked.
    Added a new funciton for form submit, didn't want to add `if's` in `getSearchResults` function
    Fixed comment on `getSearchResults` function

--- pages/resources.js
+++ pages/resources.js
@@ -30,11 +30,12 @@ export default class extends React.Component {
     logPageView()
   }
 
+  /**
+   * Get input value
+   * Make api call based on value
+   * Render cards from api data
+   */
   getSearchResults = () => {
-    /* Get input value
-        Make api call based on value
-        Render cards from api data
-     */
     const searchTerm = document.getElementById('search').value
     console.log(searchTerm)
 
@@ -42,6 +43,13 @@ export default class extends React.Component {
     const params = `resource?search=${searchTerm}&per_page=100&fields=title,acf,better_featured_image`
 
     getJSON(apiUrl + params).then(data => this.setState({ data }))
+  }
+  
+  formGetResults = (e) => {
+    e.preventDefault();
+    // form inputs can be accesed directly if they have names
+    e.target.search.blur()
+    this.getSearchResults()
   }
 
   render () {
@@ -173,22 +181,25 @@ export default class extends React.Component {
             <div className='container container-wide'>
               <div className='row'>
                 <div className='input-field col s12 m6 offset-m6'>
-                  <input
-                    id='search'
-                    type='search'
-                    onKeyUp={this.getSearchResults}
-                    style={{ width: '100%', paddingLeft: '4px' }}
-                  />
-                  <label htmlFor='search'>Search</label>
-                  <svg
-                    fill='rgba(0, 0, 0, 0.57)'
-                    height='24'
-                    viewBox='0 0 24 24'
-                    width='24'
-                  >
-                    <path d='M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z' />
-                    <path d='M0 0h24v24H0z' fill='none' />
-                  </svg>
+                  <form onSubmit={this.formGetResults}>
+                    <input
+                      name='search'
+                      id='search'
+                      onKeyUp={this.getSearchResults}
+                      type='search'
+                      style={{ width: '100%', paddingLeft: '4px' }}
+                    />
+                    <label htmlFor='search'>Search</label>
+                    <svg
+                      fill='rgba(0, 0, 0, 0.57)'
+                      height='24'
+                      viewBox='0 0 24 24'
+                      width='24'
+                    >
+                      <path d='M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z' />
+                      <path d='M0 0h24v24H0z' fill='none' />
+                    </svg>
+                  </form>
                 </div>
               </div>
               <div className='row'>
