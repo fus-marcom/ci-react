d470f237754000d3a403ac7dd61147232b1e897c
J. Patrick Lanigan <planigan@gmail.com>
2017-07-14 19:38:58 -0400

    Refactor ExpandingSection component

--- components/ExpandingSection.js
+++ components/ExpandingSection.js
@@ -15,110 +15,50 @@ class ExpandingSection extends Component {
 
   isEven = () => this.props.index % 2 === 0
 
+  ImageSection = ({ img, imgText, imgTitle }) => {
+    return (
+      <div className='col s12 m6 order-1'>
+        <img
+          className='responsive-img z-depth-1'
+          src={img}
+          alt={imgText}
+          title={imgTitle}
+        />
+      </div>
+    )
+  }
+
+  TextSection = ({ extraClasses, url, title, content }) => {
+    return (
+      <div className={`col s12 flow-text ${extraClasses}`}>
+        <span dangerouslySetInnerHTML={{ __html: content }} />
+        {url &&
+          <OutLink to={url} title={title} label={`linkTo${url}`}>
+            <button className='btn waves-effect waves-light'>Learn More</button>
+          </OutLink>}
+      </div>
+    )
+  }
+
   render () {
+    const ImageSection = this.ImageSection
+    const TextSection = this.TextSection
+    const hasImage = !!this.props.img // !! is boolean coercion
+    const moreClasses = !hasImage ? 'valign order-2' : '' // text only section gets these
+
     return (
       <div className='sub-section' key={this.props.key}>
-        {this.props.img !== ''
-          ? <div>
-            <ClickHeading
-              clickHandler={this.clickHandler}
-              displayContent={this.state.displayContent}
-              title={this.props.title}
-              />
+        <ClickHeading
+          clickHandler={this.clickHandler}
+          displayContent={this.state.displayContent}
+          title={this.props.title}
+        />
 
-            {this.state.displayContent
-                ? this.isEven()
-                  ? <div className='row valign-wrapper'>
-                    <div className='col s12 flow-text valign order-2'>
-
-                      <span
-                        dangerouslySetInnerHTML={{
-                          __html: this.props.content
-                        }}
-                        />
-                      {this.props.url &&
-                      <OutLink
-                        to={this.props.url}
-                        title={this.props.title}
-                        label={`linkTo${this.props.url}`}
-                          >
-                        <button className='btn waves-effect waves-light'>
-                              Learn More
-                            </button>
-                      </OutLink>}
-
-                    </div>
-                    <div className='col s12 m6 order-1'>
-                      <img
-                        className='responsive-img z-depth-1'
-                        src={this.props.img}
-                        alt={this.props.imgAlt}
-                        title={this.props.imgTitle}
-                        />
-                    </div>
-                  </div>
-                  : <div className='row valign-wrapper'>
-                    <div className='col s12 m6 order-1'>
-                      <img
-                        className='responsive-img z-depth-1'
-                        src={this.props.img}
-                        alt={this.props.imgAlt}
-                        title={this.props.imgTitle}
-                        />
-                    </div>
-
-                    <div className='col s12 flow-text valign order-2'>
-
-                      <span
-                        dangerouslySetInnerHTML={{
-                          __html: this.props.content
-                        }}
-                        />
-                      {this.props.url !== ''
-                          ? <OutLink
-                            to={this.props.url}
-                            title={this.props.title}
-                            label={`linkTo${this.props.url}`}
-                            >
-                            <button className='btn waves-effect waves-light'>
-                                Learn More
-                              </button>
-                          </OutLink>
-                          : ''}
-                    </div>
-
-                  </div>
-                : ''}
-
-          </div>
-          : <div>
-            <ClickHeading
-              clickHandler={this.clickHandler}
-              displayContent={this.state.displayContent}
-              title={this.props.title}
-              />
-            <div className='row valign-wrapper'>
-              {this.state.displayContent
-                  ? <div className='col s12 flow-text'>
-                    <span
-                      dangerouslySetInnerHTML={{ __html: this.props.content }}
-                      />
-                    {this.props.url !== ''
-                        ? <OutLink
-                          to={this.props.url}
-                          title={this.props.title}
-                          label={`linkTo${this.props.url}`}
-                          >
-                          <button className='btn waves-effect waves-light'>
-                              Learn More
-                            </button>
-                        </OutLink>
-                        : ''}
-                  </div>
-                  : ''}
-
-            </div>
-
+        {this.state.displayContent &&
+          <div className='row valign-wrapper'>
+            {hasImage && this.isEven() && <ImageSection {...this.props} />}
+            <TextSection extraClasses={moreClasses} {...this.props} />
+            {hasImage && !this.isEven() && <ImageSection {...this.props} />}
           </div>}
       </div>
     )
